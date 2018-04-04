2c0ef2dc02d2d244b92f4e866461ccdcc63c403d
Matt Perry <mpcomplete@gmail.com>
2018-04-04 03:16:48 -0400

    IV Coffee

---
+++ IntravenousCoffee/IVTool.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using StardewValley;
+using StardewValley.Tools;
+using StardewValley.TerrainFeatures;
+
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using StardewValley.Objects;
+using SObject = StardewValley.Object;
+using PyTK.CustomElementHandler;
+using StardewModdingAPI;
+
+namespace IntravenousCoffee
+{
+    public class IntravenousCoffeeTool : Tool, ISaveElement
+    {
+
+        internal static Texture2D texture;
+        private static Texture2D attTexture;
+        private bool inUse;
+        private IMonitor Monitor;
+
+        public Dictionary<string, string> getAdditionalSaveData()
+        {
+            Dictionary<string, string> savedata = new Dictionary<string, string>();
+            savedata.Add("name", name);
+            return savedata;
+        }
+
+        public dynamic getReplacement()
+        {
+            Chest replacement = new Chest(true);
+            if(attachments.Count() > 0)
+            {
+                if(attachments[0] != null)
+                    replacement.addItem(attachments[0]);
+            }
+            
+            return replacement;
+        }
+
+        public void rebuild(Dictionary<string, string> additionalSaveData, object replacement)
+        {
+            build();
+            Chest chest = (Chest)replacement;
+            if (!chest.isEmpty()) {
+                    attachments[0] = (SObject)chest.items[0];
+            }
+            
+        }
+
+        public IntravenousCoffeeTool()
+            : base()
+        {
+            build();
+        }
+
+        public IntravenousCoffeeTool(IMonitor monitor)
+            :base()
+        {
+            this.Monitor = monitor;
+            build();
+        }
+
+        public override string Name
+        {
+            get => name;
+        }
+
+        public override bool canBeTrashed()
+        {
+            return true;
+        }
+
+        internal static void loadTextures()
+        {
+            texture = IntravenousCoffeeMod._helper.Content.Load<Texture2D>(@"Assets/ivbag.png");
+            attTexture = IntravenousCoffeeMod._helper.Content.Load<Texture2D>(@"Assets/attachment.png");
+        }
+
+
+        public override bool actionWhenPurchased()
+        {
+            return true;
+        }
+
+        public override Item getOne()
+        {
+            return new IntravenousCoffeeTool(this.Monitor);
+        }
+
+        private void build()
+        {
+            if (texture == null)
+                loadTextures();
+
+            name = "IV bag";
+            description = "Empty";
+
+            numAttachmentSlots = 1;
+            attachments = new SObject[numAttachmentSlots];
+            initialParentTileIndex = 77;
+            currentParentTileIndex = 77;
+            indexOfMenuItemView = 0;
+            upgradeLevel = 5;
+           
+            instantUse = false;
+            inUse = false;
+        }
+
+        public override int attachmentSlots()
+        {
+            return numAttachmentSlots;
+        }
+        
+        public override void drawInMenu(SpriteBatch spriteBatch, Vector2 location, float scaleSize, float transparency, float layerDepth, bool drawStackNumber)
+        {
+            spriteBatch.Draw(texture, location + new Vector2((float)(Game1.tileSize / 2), (float)(Game1.tileSize / 2)), new Rectangle?(Game1.getSquareSourceRectForNonStandardTileSheet(texture, Game1.tileSize / 4, Game1.tileSize / 4, this.indexOfMenuItemView)), Color.White * transparency, 0f, new Vector2((float)(Game1.tileSize / 4 / 2), (float)(Game1.tileSize / 4 / 2)), (float)Game1.pixelZoom * scaleSize, SpriteEffects.None, layerDepth);
+        }
+
+        public override void drawAttachments(SpriteBatch b, int x, int y)
+        {
+            Rectangle attachementSourceRectangle = new Rectangle(0, 0, 64, 64);
+            b.Draw(attTexture, new Vector2(x, y), new Rectangle?(attachementSourceRectangle), Color.White, 0.0f, Vector2.Zero, 1f, SpriteEffects.None, 0.86f);
+
+            if (attachments.Count() > 0)
+            {
+                if (attachments[0] is SObject)
+                    attachments[0].drawInMenu(b, new Vector2(x, y), 1f);
+            }
+        }
+
+        public override bool onRelease(GameLocation location, int x, int y, StardewValley.Farmer who)
+        {
+            return false;
+        }
+
+        public override bool beginUsing(GameLocation location, int x, int y, StardewValley.Farmer who)
+        {
+            return false;
+        }
+
+        public override bool canThisBeAttached(SObject o)
+        {
+            return o == null || o.DisplayName == "Coffee";
+        }
+
+        public override SObject attach(SObject o)
+        {
+            SObject priorAttachment = null;
+
+            if (attachments[0] != null)
+                priorAttachment = new SObject(Vector2.Zero, attachments[0].parentSheetIndex, attachments[0].stack);
+
+            if (o == null) {
+                if (attachments[0] != null) {
+                    priorAttachment = new SObject(Vector2.Zero, attachments[0].parentSheetIndex, attachments[0].stack);
+                    attachments[0] = null;
+                }
+
+                Game1.playSound("dwop");
+                return priorAttachment;
+            }
+
+            if (canThisBeAttached(o)) {
+                if (attachments[0] != null) {
+                    attachments[0].stack += o.stack;
+                } else {
+                    attachments[0] = o;
+                }
+                Game1.playSound("button1");
+                return null;
+            }
+ 
+            return null;
+        }
+
+        public override void DoFunction(GameLocation location, int x, int y, int power, StardewValley.Farmer who)
+        {
+            return;
+        }
+       
+        public override string getDescription()
+        {
+            if (attachments.Count() > 0)
+            {
+                if(attachments[0] != null)
+                {
+                    return attachments[0].name;
+                }
+
+                string text = description;
+                SpriteFont smallFont = Game1.smallFont;
+                int width = Game1.tileSize * 4 + Game1.tileSize / 4;
+                return Game1.parseText(text, smallFont, width);
+            }
+            else
+            {
+                string text = description;
+                SpriteFont smallFont = Game1.smallFont;
+                int width = Game1.tileSize * 4 + Game1.tileSize / 4;
+                return Game1.parseText(text, smallFont, width);
+            }
+           
+
+        }
+
+        protected override string loadDisplayName()
+        {
+            return name;
+        }
+
+        protected override string loadDescription()
+        {
+            return getDescription();
+        }
+    }
+}
---
+++ IntravenousCoffee/ModEntry.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+
+using StardewValley.Menus;
+using PyTK.Types;
+using PyTK.Extensions;
+using Pathoschild.Stardew.Common;
+using Microsoft.Xna.Framework;
+
+namespace IntravenousCoffee
+{
+    public class IntravenousCoffeeMod : Mod
+    {
+        const int kCoffeeDurationMillis = 2 * 60 * 1000;  // 2 min
+        const int kWithdrawalDurationMillis = 2 * 60 * 60 * 1000; // 2 hours
+        const int kBuffWhich = 998;
+
+        internal static EventHandler<EventArgsClickableMenuChanged> addtoshop;
+
+        internal static IModHelper _helper;
+        internal static IMonitor _monitor;
+ 
+        int updateTicks = 60;
+
+        enum AddictionState {
+            Clean,
+            Addicted,
+            Withdrawal
+        };
+        AddictionState addiction = AddictionState.Clean;
+
+        public override void Entry(IModHelper helper)
+        {
+            _helper = helper;
+            _monitor = Monitor;
+            addtoshop = new InventoryItem(new IntravenousCoffeeTool(), 10000, 1).addToNPCShop("Pierre");
+
+            InputEvents.ButtonPressed += this.InputEvents_ButtonPressed;
+            GameEvents.UpdateTick += this.GameEvents_UpdateTick;
+        }
+
+        private void InputEvents_ButtonPressed(object sender, EventArgsInput e)
+        {
+            if (!Context.IsWorldReady)
+                return;
+
+            this.Monitor.InterceptErrors("handling your input", $"handling input '{e.Button}'", () => {
+                // HACK: This prevents the IV bag from being used as a tool. There must be a better way
+                // to do this.
+                if (e.IsUseToolButton
+                    && Game1.player.CurrentTool as IntravenousCoffeeTool != null
+                    && Game1.activeClickableMenu == null) {
+                    e.SuppressButton();
+                }
+            });
+        }
+
+        private void GameEvents_UpdateTick(object sender, EventArgs e)
+        {
+            if (updateTicks-- > 0)
+                return;
+            updateTicks = 60;
+
+            RemoveDrinkBuff(true);
+
+            // Wait till the buff runs out.
+            if (Game1.buffsDisplay.hasBuff(kBuffWhich))
+                return;
+
+            // Find an IV bag with coffee remaining.
+            IntravenousCoffeeTool ivTool = null;
+            foreach (Item item in Game1.player.items) {
+                ivTool = item as IntravenousCoffeeTool;
+                if (ivTool?.attachments[0]?.stack > 0)
+                    break;
+            }
+
+            if (ivTool != null) {
+                // Consume some coffee.
+                AddBuff(1, kCoffeeDurationMillis, "+1 Speed", "Coffee Drip");
+                if (--ivTool.attachments[0].stack == 0)
+                    ivTool.attachments[0] = null;
+                this.addiction = AddictionState.Addicted;  // caffeine's a hell of a drug
+                RemoveDrinkBuff(false);
+            } else if (this.addiction == AddictionState.Addicted) {
+                // Ran out of coffee. Go into withdrawal.
+                AddBuff(-1, kWithdrawalDurationMillis, "-1 Speed", "Coffee Withdrawal");
+                this.addiction = AddictionState.Withdrawal;  // caffeine's a hell of a drug
+            } else if (this.addiction == AddictionState.Withdrawal) {
+                // We made it out of withdrawal.
+                this.addiction = AddictionState.Clean;
+            }
+        }
+
+        private void AddBuff(int amount, int millisecondsDuration, string description, string source) {
+            if (Game1.buffsDisplay.hasBuff(kBuffWhich))
+                return;
+
+            Buff buff = new Buff(0, 0, 0, 0, 0, 0, 0, 0, 0, amount, 0, 0, 2, source, source);
+            buff.description = description;
+            buff.millisecondsDuration = millisecondsDuration;
+            buff.which = kBuffWhich;
+            buff.sheetIndex = 9;
+            if (amount > 0)
+                buff.glow = Color.Azure;
+            else
+                buff.glow = Color.Red;
+
+            Game1.buffsDisplay.addOtherBuff(buff);
+        }
+
+        private void RemoveDrinkBuff(bool warn)
+        {
+            if (this.addiction != AddictionState.Clean && Game1.buffsDisplay.drink?.source == "Coffee") {
+                Game1.buffsDisplay.drink.millisecondsDuration = 1;
+                if (warn)
+                    Game1.showRedMessage("Ingested coffee fails to satisfy your coffee addiction.");
+            }
+        }
+    }
+}
